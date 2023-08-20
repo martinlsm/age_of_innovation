febf22909b8e01806058f52566303c02576632bd
Martin Lindström <martin.lindstrom94@gmail.com>
2023-08-20 21:17:18 +0200

    Finish implementation of BuildingIncomeTrack

--- aoi_backend/src/common.rs
+++ aoi_backend/src/common.rs
@@ -1,22 +1,40 @@
-use std::ops;
+use std::{ops, iter};
 
 use enum_iterator::Sequence;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct VP(pub u32);
 
-pub trait Resource: Sized + Copy + Clone + ops::Add<Output = Self> {
-    fn from(val: u32) -> Self;
+pub trait Resource:
+    From<u32> + Copy + Clone + ops::Add<Output = Self> + ops::Sub<Output = Self>
+{
+    const IDX: usize;
+
+    fn get_val(&self) -> u32;
 }
 
 macro_rules! define_resource {
-    ($name:ident) => {
+    ($name:ident, $idx:expr) => {
         #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
         pub struct $name(pub u32);
 
         impl Resource for $name {
+            const IDX: usize = $idx;
+
+            fn get_val(&self) -> u32 {
+                self.0
+            }
+        }
+
+        impl From<u32> for $name {
             fn from(val: u32) -> Self {
                 Self(val)
+            }
+        }
+
+        impl From<$name> for u32 {
+            fn from(val: $name) -> Self {
+                val.0
             }
         }
 
@@ -27,13 +45,87 @@ macro_rules! define_resource {
                 Self(self.0 + rhs.0)
             }
         }
+
+        impl ops::Sub for $name {
+            type Output = Self;
+
+            fn sub(self, rhs: Self) -> Self::Output {
+                Self(self.0 - rhs.0)
+            }
+        }
     };
 }
 
-define_resource!(Tools);
-define_resource!(Coins);
-define_resource!(Scholars);
-define_resource!(Books);
+define_resource!(Tools, 0);
+define_resource!(Coins, 1);
+define_resource!(Scholars, 2);
+define_resource!(Books, 3);
+define_resource!(Power, 4);
+const NUM_RESOURCES: usize = 5;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Resources {
+    amounts: Vec<u32>, // Resources are stored in their respective Resource::IDX.
+}
+
+impl Resources {
+    pub fn new() -> Self {
+        Self {
+            amounts: iter::repeat(0).take(NUM_RESOURCES).collect()
+        }
+    }
+
+    pub fn get<T: Resource>(&self) -> T {
+        T::from(self.amounts[T::IDX])
+    }
+
+    pub fn gain<T: Resource>(&mut self, resource: &T) -> T {
+        self.amounts[T::IDX] += resource.get_val();
+
+        self.get()
+    }
+
+    pub fn lose<T: Resource>(&mut self, resource: &T) -> T {
+        self.amounts[T::IDX] -= resource.get_val();
+
+        self.get()
+    }
+}
+
+impl<T: Resource> From<T> for Resources {
+    fn from(value: T) -> Self {
+        let mut res = Resources::new();
+        res.amounts[T::IDX] = value.get_val();
+
+        res
+    }
+}
+
+impl ops::Add<&Resources> for Resources {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self::Output {
+        // Pointwise addition.
+        let amounts: Vec<u32> = std::iter::zip(self.amounts.iter(), rhs.amounts.iter())
+            .map(|(a, b)| a + b)
+            .collect();
+
+        Self { amounts }
+    }
+}
+
+impl ops::Sub<&Resources> for Resources {
+    type Output = Self;
+
+    fn sub(self, rhs: &Self) -> Self::Output {
+        // Pointwise subtraction.
+        let amounts: Vec<u32> = std::iter::zip(self.amounts.iter(), rhs.amounts.iter())
+            .map(|(a, b)| a - b)
+            .collect();
+
+        Self { amounts }
+    }
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Discipline {
--- aoi_backend/src/faction.rs
+++ aoi_backend/src/faction.rs
@@ -1,6 +1,5 @@
-use std::fs::create_dir;
-
-use crate::common::{Color, Resource, Tools};
+use crate::building::BuildingType;
+use crate::common::{self, Coins, Color, Resource, Resources, Tools};
 use crate::Result;
 
 use crate::error::create_error;
@@ -26,66 +25,107 @@ impl Faction {
     }
 }
 
-trait Length {
-    const Len: usize;
+pub struct BuildingIncomeTrack {
+    income_gain: Vec<Resources>, // zeroth index is base income (not any buildings placed)
+    num_occupied: usize,         // Number of occupied building slots on the income track
 }
 
-impl Track for BuildingIncomeTrack<Tools> {
-    const Len: usize = 10;
-}
-
-pub struct BuildingIncomeTrack<T: Resource> {
-    income_gain: [T; BuildingIncomeTrack::Len], // The zeroth index is the amount gained when zero workshops are placed
-    workshops_on_track: usize,
-}
-
-impl<T: Resource> BuildingIncomeTrack<T> {
-    pub fn new(color: Color) -> Self {
-        BuildingIncomeTrack {
-            // TODO: Income is currently unknown so this is a placeholder; fix when info is available
-            income_gain: [
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-                T::from(1),
-            ],
-            workshops_on_track: 9,
+impl BuildingIncomeTrack {
+    pub fn new(color: &Color, building: &BuildingType) -> Self {
+        match (color, building) {
+            (Color::Yellow, BuildingType::Workshop) => Self {
+                income_gain: vec![
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                    Resources::from(Tools(1)),
+                ],
+                num_occupied: 9,
+            },
+            (Color::Yellow, BuildingType::Guild) => todo!(),
+            (Color::Yellow, BuildingType::School) => todo!(),
+            (Color::Yellow, BuildingType::University) => todo!(),
+            (Color::Yellow, BuildingType::Palace) => todo!(),
+            (Color::Yellow, BuildingType::Tower) => todo!(),
+            (Color::Yellow, BuildingType::Monument) => todo!(),
+            (Color::Brown, BuildingType::Workshop) => todo!(),
+            (Color::Brown, BuildingType::Guild) => todo!(),
+            (Color::Brown, BuildingType::School) => todo!(),
+            (Color::Brown, BuildingType::University) => todo!(),
+            (Color::Brown, BuildingType::Palace) => todo!(),
+            (Color::Brown, BuildingType::Tower) => todo!(),
+            (Color::Brown, BuildingType::Monument) => todo!(),
+            (Color::Black, BuildingType::Workshop) => todo!(),
+            (Color::Black, BuildingType::Guild) => todo!(),
+            (Color::Black, BuildingType::School) => todo!(),
+            (Color::Black, BuildingType::University) => todo!(),
+            (Color::Black, BuildingType::Palace) => todo!(),
+            (Color::Black, BuildingType::Tower) => todo!(),
+            (Color::Black, BuildingType::Monument) => todo!(),
+            (Color::Blue, BuildingType::Workshop) => todo!(),
+            (Color::Blue, BuildingType::Guild) => todo!(),
+            (Color::Blue, BuildingType::School) => todo!(),
+            (Color::Blue, BuildingType::University) => todo!(),
+            (Color::Blue, BuildingType::Palace) => todo!(),
+            (Color::Blue, BuildingType::Tower) => todo!(),
+            (Color::Blue, BuildingType::Monument) => todo!(),
+            (Color::Green, BuildingType::Workshop) => todo!(),
+            (Color::Green, BuildingType::Guild) => todo!(),
+            (Color::Green, BuildingType::School) => todo!(),
+            (Color::Green, BuildingType::University) => todo!(),
+            (Color::Green, BuildingType::Palace) => todo!(),
+            (Color::Green, BuildingType::Tower) => todo!(),
+            (Color::Green, BuildingType::Monument) => todo!(),
+            (Color::Gray, BuildingType::Workshop) => todo!(),
+            (Color::Gray, BuildingType::Guild) => todo!(),
+            (Color::Gray, BuildingType::School) => todo!(),
+            (Color::Gray, BuildingType::University) => todo!(),
+            (Color::Gray, BuildingType::Palace) => todo!(),
+            (Color::Gray, BuildingType::Tower) => todo!(),
+            (Color::Gray, BuildingType::Monument) => todo!(),
+            (Color::Red, BuildingType::Workshop) => todo!(),
+            (Color::Red, BuildingType::Guild) => todo!(),
+            (Color::Red, BuildingType::School) => todo!(),
+            (Color::Red, BuildingType::University) => todo!(),
+            (Color::Red, BuildingType::Palace) => todo!(),
+            (Color::Red, BuildingType::Tower) => todo!(),
+            (Color::Red, BuildingType::Monument) => todo!(),
         }
     }
 
     pub fn remove_building(&mut self) -> Result<()> {
-        if self.workshops_on_track == 0 {
+        if self.num_occupied == 0 {
             Err(create_error("No workshops left on board"))
         } else {
-            self.workshops_on_track -= 1;
+            self.num_occupied -= 1;
 
             Ok(())
         }
     }
 
     pub fn put_building(&mut self) -> Result<()> {
-        if self.workshops_on_track >= 9 {
+        if self.num_occupied >= 9 {
             Err(create_error("Board is already full of workshops"))
         } else {
-            self.workshops_on_track += 1;
+            self.num_occupied += 1;
 
             Ok(())
         }
     }
 
-    pub fn income(&self) -> T {
-        let num_income_slots = self.income_gain.len() - self.workshops_on_track;
+    pub fn income(&self) -> Resources {
+        let num_income_slots = self.income_gain.len() - self.num_occupied;
 
         self.income_gain
             .iter()
             .take(num_income_slots)
-            .fold(T::from(0), |a, b| a + *b)
+            .fold(Resources::new(), |a, b| a + b)
     }
 }
 
@@ -95,20 +135,44 @@ mod tests {
 
     #[test]
     fn income_for_zero_workshops() {
-        let track : BuildingIncomeTrack<Tools> = BuildingIncomeTrack::new(Color::Black); // Arbitrary color
+        let track: BuildingIncomeTrack =
+            BuildingIncomeTrack::new(&Color::Yellow, &BuildingType::Workshop); // Arbitrary color
 
-        assert_eq!(track.income(), Tools(1));
+        assert_eq!(track.income(), Resources::from(Tools(1)));
     }
 
     #[test]
     fn income_for_two_workshops() -> Result<()> {
-        let mut track: BuildingIncomeTrack<Tools> = BuildingIncomeTrack::new(Color::Blue); // Arbitrary color
+        let mut track: BuildingIncomeTrack =
+            BuildingIncomeTrack::new(&Color::Yellow, &BuildingType::Workshop); // Arbitrary color
 
         track.remove_building()?;
         track.remove_building()?;
 
-        assert_eq!(track.income(), Tools(3));
+        assert_eq!(track.income(), Resources::from(Tools(3)));
 
         Ok(())
+    }
+
+    #[test]
+    fn remove_to_many_from_income_track() -> Result<()> {
+        let mut track = BuildingIncomeTrack::new(&Color::Yellow, &BuildingType::Workshop);
+
+        for _ in 0..9 {
+            track.remove_building()?;
+        }
+
+        assert!(track.remove_building().is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn put_building_on_track_when_full() -> Result<()> {
+        let mut track = BuildingIncomeTrack::new(&Color::Yellow, &BuildingType::Workshop);
+
+        assert!(track.put_building().is_err());
+
+       Ok(())
     }
 }
